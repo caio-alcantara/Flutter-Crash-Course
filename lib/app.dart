7b9d8a5bb5df842ed2acf3fae297ea2f312590f4
Caio de Alcantara Santos <95832094+caio-alcantara@users.noreply.github.com>
2025-02-22 14:25:21 -0300

    feat: adds styles widgtes and assets

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course/screens/location_detail/location_detail.dart';
-
+import 'package:flutter_course/style.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LocationDetail());
+    return MaterialApp(
+      home: LocationDetail(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          titleTextStyle: AppBarTextStyle,
+        ),
+        textTheme: TextTheme(
+          titleLarge: TitleLargeTextStyle,
+          bodyLarge: BodyLargeTextStyle,
+        )
+      ),
+    );
   }
 }
--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,23 +1,24 @@
 // screens/location_detail/location_detail.dart
 
 import 'package:flutter/material.dart';
+import 'package:flutter_course/screens/location_detail/image_banner.dart';
 import 'package:flutter_course/screens/location_detail/text_section.dart';
-
 
 class LocationDetail extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Crash Course'),
+        title: Text('Location Detail'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          TextSection(Colors.red, ''),
-          TextSection(Colors.green, ''),
-          TextSection(Colors.blue, ''),
+          ImageBanner("assets/images/kiyomizu-dera.jpg"),
+          TextSection("sumary1", "something1"),
+          TextSection("sumary2", "something2"),
+          TextSection("sumary3", "something3"),
         ],
       ),
     );
